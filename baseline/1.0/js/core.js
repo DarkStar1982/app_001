fcf7a65bd24c9bd2da5619699c9f60b0220a0dfd
Dennis <d_silin@yahoo.com>
2014-08-09 17:31:09 -0400

    Added row addition

--- baseline/1.0/js/core.js
+++ baseline/1.0/js/core.js
@@ -8,8 +8,10 @@ var namespace_portfolio = (function()
     };
 
     /* Private methods */
-    function recompute_portfolio()
+    function compute_positions()
     {
+        //sort transactions
+        //now co
     }
 
     /* Public methods */
@@ -23,21 +25,43 @@ var namespace_portfolio = (function()
             //else pop an error message
             state.transactions.push(p_action);
             //update positions
-            var transactions = get_portfolio_transactions();
-            state.positions = compute_positions(transactions);
-            state.net_positions = compute_net_positions(transactions);
+            //state.positions = compute_positions();
+            //state.net_positions = compute_net_positions(transactions);
             //update_net values
-            alert(state.transactions[0].deposit_date);
             return state;
         }
     };
 }) ();
 
+// GUI update code
 var namespace_gui = (function() {
+    /* Private */
+    function create_transaction_row(obj)
+    {
+        var new_row = '<tr><td class="asset_name">'+ obj.asset
+            + '</td><td class="sector_label">' + obj.sector 
+            + '</td><td class="buysell_label">' + obj.type
+            + '</td><td class="volume_label">'+ obj.volume
+            + '</td><td class="book_date">' + obj.book_date
+            + '</td><td class="book_price">' + obj.book_price
+            + '</td><td class="current_price">'+ obj.last_price
+            + '</td><td><button onclick="remove_row_action(this)" class="btn">Remove</button></td></tr>';
+       return new_row;
+    }
+    /* Public */ 
     return {
-        render_page: function(portfolio_object)
+        render_page: function(portfolio)
         {
+            //depending on state....
+            $("#matrix").empty();
+            for (var i=0; i <portfolio.transactions.length; i++)
+            {
+                  var str_row = create_transaction_row(portfolio.transactions[i]);
+                  $("#matrix").append(str_row);
+            }
             //add positions
+            //add net positions
+            //render charts and dashboard
             //alert (portfolio_object.cash_value);
         }
     };
--- baseline/1.0/js/events.js
+++ baseline/1.0/js/events.js
@@ -3,9 +3,13 @@ var namespace_events = (function () {
         deposit_cash: function()
         {
             var new_transaction = {
-                cash_value: $("#portfolio_cash").val(), 
-                deposit_date: datetime_util.adjust_date($("#portfolio_date").datepicker("getDate")),
-                type: "cash_deposit",
+                volume: $("#portfolio_cash").val(), 
+                book_date: datetime_util.adjust_date($("#portfolio_date").datepicker("getDate")),
+                type: "Deposit",
+                asset: "Cash",
+                sector: "-",
+                book_price: 1.0,
+                last_price: 1.0,
                 flags: ['first']
             };
             namespace_gui.render_page(namespace_portfolio.update_state(new_transaction)); 
