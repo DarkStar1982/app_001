d44c5c7eb00e2750163f83f0bcc0ff0d431f2c01
Dennis <d_silin@yahoo.com>
2015-07-08 20:19:35 -0400

    First page finished

--- baseline/js/basic.js
+++ baseline/js/basic.js
@@ -14,27 +14,51 @@ $(document).ready(function(){
 });
 
 var namespace_basic = (function(e) {
+
+  function map_to_html(score, p_obj)
+  {
+    var template='<table class="table table-bordered">'+
+    '<tr><th>Score</th><th>Your&nbsp;Investment&nbsp;Profile</th><th>Notes</th><th>Actions</th></tr>'+
+    '<tr><td>'
+      + score + '</td><td>' + p_obj["title"] + '</td><td>'
+      + p_obj["Description"]
+      + '</td><td><button class="btn btn-default">Show Investment Plans</button></td></tr></table>';
+    $("#investor_type").empty();
+    $("#investor_type").append(template);
+  }
+
   return {
     //Compute portfolio performance
     display_score: function(p_rank_value)
     {
-      //8-15
+      var investor_profile = {};
       if (p_rank_value<16)
       {
-        //security
+        investor_profile["title"] = "Conservative&nbsp;Investor";
+        investor_profile["Description"] = "You primary objective is the preservation of capital with minimal risk."
+          + "You can allow only short amount of time for portfolio to recover from negative period";
       }
       else if (p_rank_value<24)
-        //income
+      {
+        //balanced
+        investor_profile["title"] = "Income&nbsp;Investor";
+        investor_profile["Description"] = "You are willing to tolerate some market fluctuations for the better returns.";
       }
       else if (p_rank_value>24)
+      {
         //growth
+        investor_profile["title"] = "Growth&nbsp;Investor";
+        investor_profile["Description"] = "You are willing to tolerate market fluctuations and more time for the market to recover value."
+          + "You have some experience with investments and you understand tradeoff betwee market risks and returns.";
       }
       else if (p_rank_value>30)
-        //income
+      {
+        //aggressive growth
+        investor_profile["title"] = "Aggressive&nbsp;Growth&nbsp;Investor";
+        investor_profile["Description"] = "You are a knowledgeable investor and not concerned about short-term fluctuations"
+          + "You are willing to wait longer time to maximize your profits.";
       }
-      console.log(p_rank_value);
-    //  switch (p_rank_value)
-
+      map_to_html(p_rank_value, investor_profile);
     }
   }
 }) ();
