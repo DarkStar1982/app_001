d87c76ef6cec4af58fedf6c8eb45e46ba020cbe4
Dennis <d_silin@yahoo.com>
2015-08-02 19:23:25 -0400

    Adding portfolio codes

--- baseline/js/basic.js
+++ baseline/js/basic.js
@@ -45,10 +45,154 @@ var namespace_marketdata = (function(){
 
 var namespace_portfolio_aux = (function(){
   return {
-    compute_transactions: function()
+    compute_net_data: function(position_data)
     {
-      return 0:
-    }
+        var total_cash = position_data.total_cash;
+        var start_cash = position_data.start_cash;
+        var cash_change = parseFloat(total_cash - start_cash);
+        var position_list =[];
+        var profit_or_loss = 0.0;
+        var net_profit = 0.0;
+        var total_pnl = 0.0;
+        var net_value = 0.0;
+        var net_data = position_data.net_positions;
+        for (var k in net_data)
+        {
+            if (net_data.hasOwnProperty(k))
+            {
+                var avg_price = parseFloat(net_data[k][1] / net_data[k][0]);
+                if (k.indexOf("_S")>-1)
+                {
+                    profit_or_loss = - parseFloat(net_data[k][2] - net_data[k][1]);
+                    net_value = profit_or_loss + net_value;
+                }
+                else
+                {
+                    profit_or_loss = parseFloat(net_data[k][2] - net_data[k][1]);
+                    net_value = net_data[k][2] + net_value;
+                }
+                if (net_data[k][0]>0)
+                {
+                    position_list.push({"symbol": k,
+                                        "volume": net_data[k][0],
+                                        "price_avg": math_util.aux_math_round(avg_price,2),
+                                        "book_value": math_util.aux_math_round(net_data[k][1],2),
+                                        "last_value": math_util.aux_math_round(net_data[k][2],2),
+                                        "pnl": math_util.aux_math_round(profit_or_loss,2),
+                                        "pnl_rel":math_util.aux_math_round(profit_or_loss / net_data[k][1] * 100.0, 2),
+                                        "sector":net_data[k][3]
+                    });
+                }
+                total_pnl = total_pnl + profit_or_loss;
+             }
+        }
+        if (position_list.length == 0)
+        {
+            var cash_row = { "start_cash": start_cash, "total_cash":total_cash, "cash_change": "-"};
+            var end_totals = total_cash;
+            total_pnl = "-";
+        }
+        else
+        {
+            var cash_row = { "start_cash": start_cash, "total_cash":total_cash, "cash_change": "-" };
+            var end_totals = math_util.aux_math_round(total_cash + net_value,2);
+            total_pnl = math_util.aux_math_round(total_pnl,2);
+        }
+        return {"positions": position_list, "net_cash_row":cash_row, "total_value" : end_totals, "total_pnl": total_pnl};
+    },
+
+    compute_position_data: function(p_transactions)
+    {
+        var total_cash = 0.0;
+        var start_cash = 0.0;
+        var net_data = new Object();
+        for (var i=0;i<p_transactions.length;i++)
+        {
+            var row_data = p_transactions[i];
+            switch (row_data.type)
+            {
+                case "Deposit":
+                    total_cash = total_cash + parseFloat(row_data.volume)*parseFloat(row_data.book_price);
+                    start_cash = total_cash;
+                    break;
+                case "Withdraw":
+                    total_cash = total_cash - parseFloat(row_data.volume)*parseFloat(row_data.book_price);
+                    break;
+                case "Buy":
+                    var book_value = parseFloat(row_data.volume)*parseFloat(row_data.book_price);
+                    var last_value = parseFloat(row_data.volume)*parseFloat(row_data.last_price);
+                    total_cash =  total_cash - book_value;
+                    var hash_value = row_data.asset+'_B';
+                    if (net_data[hash_value]==undefined)
+                    {
+                        net_data[hash_value] = [row_data.volume, book_value, last_value, row_data.sector];
+                    }
+                    else
+                    {
+                        net_data[hash_value][0] = parseInt(net_data[hash_value][0]) + parseInt(row_data.volume);
+                        net_data[hash_value][1] = parseFloat(net_data[hash_value][1] + book_value);
+                        net_data[hash_value][2] = parseFloat(net_data[hash_value][2] + last_value);
+                    }
+                    break;
+                case "Sell":
+                    var book_value = parseFloat(row_data.volume)*parseFloat(row_data.book_price);
+                    var hash_value = row_data.asset+'_B';
+                    if (net_data[hash_value] != undefined)
+                    {
+                        var last_price = parseFloat(net_data[hash_value][2]/net_data[hash_value][0]);
+                        var book_price = parseFloat(net_data[hash_value][1]/net_data[hash_value][0]);
+                        var volum_diff = parseInt(net_data[hash_value][0]) - parseInt(row_data.volume);
+                        if (volum_diff>=0)
+                        {
+                            total_cash = total_cash + book_value;
+                            net_data[hash_value][0] = volum_diff;
+                            net_data[hash_value][1] = parseFloat(net_data[hash_value][0] * book_price);
+                            net_data[hash_value][2] = parseFloat(net_data[hash_value][0] * last_price);
+                        }
+                        else console.log("not a valid action - can't sell more then you hold");
+                    }
+                    else console.log("not a valid action - can't sell what you don't have");
+                    break;
+                case "Short":
+                    var book_value = parseFloat(row_data.volume)*parseFloat(row_data.book_price);
+                    var last_value = parseFloat(row_data.volume)*parseFloat(row_data.last_price);
+                    var hash_value = row_data.asset+'_S';
+                    total_cash =  total_cash - book_value;
+                    if (net_data[hash_value]==undefined)
+                    {
+                        net_data[hash_value]= [row_data.volume, book_value, last_value, row_data.sector];
+                    }
+                    else
+                    {
+                        net_data[hash_value][0] = parseInt(net_data[hash_value][0]) + parseInt(row_data.volume);
+                        net_data[hash_value][1] = parseFloat(net_data[hash_value][1] + book_value);
+                        net_data[hash_value][2] = parseFloat(net_data[hash_value][2] + last_value);
+                    }
+                    break;
+                case "Cover":
+                    var hash_value = row_data.asset+'_S';
+                    if (net_data[hash_value] != undefined)
+                    {
+                        var last_price = parseFloat(net_data[hash_value][2]/net_data[hash_value][0]);
+                        var book_price = parseFloat(net_data[hash_value][1]/net_data[hash_value][0]);
+                        var volum_diff = parseInt(net_data[hash_value][0]) - parseInt(row_data.volume);
+                        var book_value = row_data.volume*book_price;
+                        if (volum_diff>=0)
+                        {
+                            net_data[hash_value][0] = volum_diff;
+                            net_data[hash_value][1] = parseFloat(net_data[hash_value][0] * book_price);
+                            total_cash = total_cash + book_value - row_data.volume*(row_data.book_price - book_price);
+                            net_data[hash_value][2] = parseFloat(net_data[hash_value][0] * last_price);
+                        }
+                        else console.log("Can't cover more than you have in a short position" );
+                    }
+                    else console.log("No short position found to cover");
+                    break;
+            }
+        }
+        //namespace_gui.send_log_message("start cash is "+  start_cash, "Info");
+        return { "net_positions" : net_data, "total_cash": math_util.aux_math_round(total_cash,2), "start_cash" : start_cash };
+      }
   }
 }) ();
 
@@ -327,16 +471,7 @@ var namespace_iplanner = (function(){
           if (data.header.error_code == 0)
           {
             var book_price = data.contents.price;
-            /* var new_transaction = {
-              volume: math_util.aux_math_round(value[1]/data.contents.price,0),
-              book_date: start_date,
-              type: "Buy",
-              asset: value[0],
-              sector: undefined,
-              book_price: data.contents.price,
-              last_price: undefined,
-            };
-            net_cash_value = net_cash_value + new_transaction.volume*data.contents.price; */
+            var book_volume = math_util.aux_math_round(value[1]/data.contents.price,0);
             var last_date = datetime_util.adjust_date(datetime_util.get_yesterday_date());
             $.getJSON(API_URL, {instrument:value[0], call:"quote", datetime:last_date}, function(data)
             {
@@ -348,33 +483,42 @@ var namespace_iplanner = (function(){
                   if (data.header.error_code == 0)
                   {
                     //here be transaction
+                    var sector_value = data.contents.sector_data;
+                    var new_transaction = {
+                      volume:book_volume,
+                      book_date: start_date,
+                      type: "Buy",
+                      asset: value[0],
+                      sector: sector_value,
+                      book_price: book_price,
+                      last_price: last_price,
+                    };
+                    net_cash_value = net_cash_value + new_transaction.volume*new_transaction.book_price;
+                    transaction_list.push(new_transaction);
+                    counter--;
+                    if (counter<=0)
+                    {
+                      var cash_transaction = {
+                        volume: net_cash_value,
+                        book_date: start_date,
+                        type: "Deposit",
+                        asset: "Cash",
+                        sector: "-",
+                        book_price: 1.0,
+                        last_price: 1.0
+                      };
+                      transaction_list.push(cash_transaction);
+                      console.log(transaction_list);
+                      var position_data = namespace_portfolio_aux.compute_position_data(transaction_list);
+                      console.log(position_data);
+                    }
                   }
-                  counter--;
                 });
               }
             });
           }
-
-          //build a portfolio
-          //console.log([start_date, value[0], shares]);
-        //  console.log(net_cash_value);
-          if (counter<=0)
-          {
-            var cash_transaction = {
-              volume: net_cash_value,
-              book_date: start_date,
-              type: "Deposit",
-              asset: "Cash",
-              sector: "-",
-              book_price: 1.0,
-              last_price: 1.0
-            }
-            transaction_list.push(cash_transaction);
-          }
         });
       })
-      //console.log(p_data_extended["basic"]);
-      //console.log("Basic selected");
     }
     else if ($("input[name=portfolio_selected]:checked").val()=="advanced")
     {
